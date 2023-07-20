3bc74c5ecda6e7ab41dfae81d530820bb4ac09d0
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-07-20 20:45:57 -0300

    req 2

--- src/index.js
+++ src/index.js
@@ -37,3 +37,16 @@ const lendoArquivo = async () => {
       res.status(500).send({ message: err.message });
     }
   });
+
+  app.get('/talker/:id', async (req, res) => {
+    try {
+      const talkers = await lendoArquivo();
+      const talker = talkers.find(({ id }) => id === Number(req.params.id));
+      if (!talker) {
+        return res.status(404).json({ message: "Pessoa palestrante não encontrada" });
+      }
+      res.status(200).json(talker);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  });
