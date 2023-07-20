838c4dcaac06b3a5cd6fa8d588cbd5a29f520346
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-07-20 20:35:19 -0300

    req 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
+
+const arquivo = path.join(__dirname, 'talker.json');
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -14,3 +18,22 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+const lendoArquivo = async () => {
+  try {
+    const data = await fs.readFile(arquivo);
+    const talker = JSON.parse(data);
+    return talker;
+  } catch (error) {
+    console.error(`Erro ao tentar lê o arquivo: ${error.message}`);
+  }
+  };
+
+  app.get('/talker', async (req, res) => {
+    try {
+      const talker = await lendoArquivo();
+      res.status(200).json(talker);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  });
